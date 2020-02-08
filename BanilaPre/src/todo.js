0640119febdaa28657d9ad4f8b68ec47f3320a29
supercoder <kazehazaya@gmail.com>
2020-02-08 20:43:59 +0900

    asdasdsa
    dasdasdsadsad

---
+++ BanilaPre/src/todo.js
@@ -0,0 +1,7 @@
+const toDoFrom = document.querySelector(".js-toDoFrom"),
+    toDoInput = form.querySelector("input"),
+    toDoList = document.querySelector(".js-toDolist");
+
+    function init(){
+        
+    }
